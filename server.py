f5978a87b20bf056861c1ce3ccb051c8a4204eae
Melody Sue <melodysue@WL-209-170.WPA.Claremont.Edu>
2015-04-11 00:40:08 -0700

    D:

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, url_for
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = ".")
 
 
 @app.route('/')
 def map():
-	return render_template('map.html')
+	return render_template('example.html')
 
 app.run(debug=True)
